e5662b2fa62dc04a3df1afb1f0a5c363cefa3d2b
tmont <tommy.mont@gmail.com>
2011-06-17 01:00:28 +0000

    renamed RouteTest to RouteTests

--- tests/Facilius/RouteTests.php
+++ tests/Facilius/RouteTests.php
@@ -5,7 +5,7 @@
 	use Facilius\Route;
 	use PHPUnit_Framework_TestCase;
 
-	class RouteTest extends PHPUnit_Framework_TestCase {
+	class RouteTests extends PHPUnit_Framework_TestCase {
 		public function testShouldGenerateUrl() {
 			self::markTestSkipped('not implemented yet');
 		}
